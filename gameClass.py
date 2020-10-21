1555b4031c6bc82b27e585822d5393cfab6d4908
bsstibich <bsstibich@gmail.com>
2020-10-21 16:27:23 -0500

    Optimize lines that dictate which card gets played from hand

--- gameClass.py
+++ gameClass.py
@@ -48,30 +48,16 @@ class Game:
 			print("Enter the card number you wish to play\nHit Enter to end turn\nEnter 99 to end game")
 			try:
 				inp = input("Enter Your Choice: ")
-				if inp == str(0):
-					self.board.play(self.deck.hand, 0)
-				elif inp == str(1):
-					self.board.play(self.deck.hand, 1)
-				elif inp == str(2):
-					self.board.play(self.deck.hand, 2)
-				elif inp == str(3):
-					self.board.play(self.deck.hand, 3)
-				elif inp == str(4):
-					self.board.play(self.deck.hand, 4)
-				elif inp == str(5):
-					self.board.play(self.deck.hand, 5)
-				elif inp == str(6):
-					self.board.play(self.deck.hand, 6)
-				elif inp == str(7):
-					self.board.play(self.deck.hand, 7)
-				elif inp == str(8):
-					self.board.play(self.deck.hand, 8)
-				elif inp == "":
+				if inp == "":
 					self.deck.draw(1)
 					self.board.checkSpells()
 					turn += 1
-				elif inp == str(99):
-					running = False
+				else:
+					inp = int(inp)
+					if inp == 99:
+						running = False
+					else:
+						self.board.play(self.deck.hand, inp)
 			except IndexError:
 				print("Invalid Input")
 
